61d1b987661fbd4deeb1b5f7266d9e1893fba3ef
Mónica <Mnica>
2017-03-10 17:46:14 -0600

    Affiliates con seed

--- database/seeds/AffiliatesTableSeeder.php
+++ database/seeds/AffiliatesTableSeeder.php
@@ -11,6 +11,11 @@ class AffiliatesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('affiliates')->insert([
+            'name'=>'The Green Tea House Bar',
+            'link'=>'https://www.facebook.com/TheGreenTeaHouseBar/',
+            'image'=>'',
+            'description'=>'Difundir los beneficios del té verde y la herbolaria mexicana',
+        ]);
     }
 }
