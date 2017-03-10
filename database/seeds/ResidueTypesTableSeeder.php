32e14184282697624cfbb4fb6cd92f2c4b953f4d
Angel Roberto Ruiz Mendoza <Angel Roberto Ruiz Mendoza>
2017-03-10 17:58:07 -0600

    Seeder incompleto de Residue Types

--- database/seeds/ResidueTypesTableSeeder.php
+++ database/seeds/ResidueTypesTableSeeder.php
@@ -11,6 +11,28 @@ class ResidueTypesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('residue_types')->insert([
+        	'name' => 'PET',
+        	'description' => 'Botellas de plástico, frascos de plástico, cintas de video, radiografías, etc',
+        	'image' => '/img/pet.png',
+        ]);
+
+        DB::table('residue_types')->insert([
+        	'name' => 'Papel',
+        	'description' => 'Carton, hojas de papel, servilletas, toallas de papel, etc. PAPEL DE BAÑO NO',
+        	'image' => '/img/pet.png',
+        ]);
+
+        DB::table('residue_types')->insert([
+        	'name' => 'Aluminio',
+        	'description' => 'Papel aluminio, latas de refresco, latas de no perecederos, bandejas de aluminio, etc',
+        	'image' => '/img/pet.png',
+        ]);
+
+        DB::table('residue_types')->insert([
+        	'name' => 'Vidrio',
+        	'description' => 'Botellas de cristal, tarros de cristal, vasos, etc. NINGUN TIPO DE CONTENEDOR CON RESIDUOS PELIGROSOS',
+        	'image' => '/img/pet.png',
+        ]);
     }
 }
