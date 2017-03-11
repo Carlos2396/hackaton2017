033d19f3751c63d08f32be4053b8c17c8bd76b12
Carlos2396 <carlosaam96@gmail.com>
2017-03-10 18:35:45 -0600

    Se agregaron las plantillas de las vista

---
+++ resources/views/index.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.master')
+
+@section('content')
+    <section class="no-mb">
+        <div class="jumbotron">
+
+            <div class="dark-mask"></div>
+
+            <div class="container">
+                <div class="row mb-small">
+                    <div class="col-sm-12 text-center">
+                        <h1>Universal is great</h1>
+                        <h2>Last week more than 100 companies signed up to help them in their projects.</h2>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6 mb-small">
+                        <img class="img-responsive" src="img/template-mac.png" alt="">
+                    </div>
+
+                    <div class="col-sm-6 text-center-xs">
+                        <p class="text-uppercase">Business. Corporate. Agency.
+                            <br />Portfolio. Blog. E-commerce.
+                            <br />We have covered everything.</p>
+                        <p>See our current packages comparison
+                            <br />to choose the right one for you.</p>
+                        <p><a href="#packages" class="scroll-to btn btn-template-transparent-black">See packages</a>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bar background-gray no-mb padding-big text-center-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="text-uppercase">Everything you need</h2>
+                    <p class="lead mb-small">40+ prepared HTML templates</p>
+                    <p class="mb-small">We have prepared literally <strong>everything you would possibly need building corporate, e-commerce or portfolio website</strong>. If you still miss something, let us know and we will try to include it in theme's future updates.</p>
+
+                    <p><a href="#" class="btn btn-template-main">Read more</a>
+                    </p>
+                </div>
+                <div class="col-md-6 text-center">
+                    <img src="img/template-easy-customize.png" alt="" class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bar no-mb color-white padding-big text-center-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <img src="img/template-easy-code.png" alt="" class="img-responsive">
+                </div>
+                <div class="col-md-6">
+                    <h2 class="text-uppercase">Clean, valid and documented code &amp; support</h2>
+                    <p class="lead mb-small">We made our best to help you</p>
+                    <p class="mb-small">Everything in this template is properly named and categorized. In case you don't find in the documentation answer to your question, our support is here to help!</p>
+
+                    <p><a href="#" class="btn btn-template-transparent-primary">Read more</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bar background-gray no-mb padding-big text-center-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="text-uppercase">Easy to customize</h2>
+                    <p class="mb-small">We have made this template with focus on future client customization. You get <strong>40+ prepared HTML templates</strong>, many <strong>reusable components</strong> and <strong>LESS files</strong> for even easier template customization.</p>
+
+                    <p><a href="#" class="btn btn-template-main">Read more</a>
+                    </p>
+                </div>
+                <div class="col-md-6 text-center">
+                    <img src="img/template-easy-customize.png" alt="" class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bar background-gray no-mb">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="owl-carousel customers">
+                        <li class="item">
+                            <img src="img/customer-1.png" alt="" class="img-responsive">
+                        </li>
+                        <li class="item">
+                            <img src="img/customer-2.png" alt="" class="img-responsive">
+                        </li>
+                        <li class="item">
+                            <img src="img/customer-3.png" alt="" class="img-responsive">
+                        </li>
+                        <li class="item">
+                            <img src="img/customer-4.png" alt="" class="img-responsive">
+                        </li>
+                        <li class="item">
+                            <img src="img/customer-5.png" alt="" class="img-responsive">
+                        </li>
+                        <li class="item">
+                            <img src="img/customer-6.png" alt="" class="img-responsive">
+                        </li>
+                    </ul>
+                    <!-- /.owl-carousel -->
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+@endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,41 @@
+<div class="navbar-affixed-top" data-spy="affix" data-offset-top="200">
+
+    <div class="navbar navbar-default yamm" role="navigation" id="navbar">
+
+        <div class="container">
+            <div class="navbar-header">
+
+                <a class="navbar-brand home" href="index.html">
+                    <img src="img/logo.png" alt="Universal logo" class="hidden-xs hidden-sm">
+                    <img src="img/logo-small.png" alt="Universal logo" class="visible-xs visible-sm"><span class="sr-only">Universal - go to homepage</span>
+                </a>
+                <div class="navbar-buttons">
+                    <button type="button" class="navbar-toggle btn-template-main" data-toggle="collapse" data-target="#navigation">
+                        <span class="sr-only">Toggle navigation</span>
+                        <i class="fa fa-align-justify"></i>
+                    </button>
+                </div>
+            </div>
+            <!--/.navbar-header -->
+
+            <div class="navbar-collapse collapse" id="navigation">
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown active">
+                        <a href="javascript: void(0)" class="dropdown-toggle" data-toggle="dropdown">Home <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="index.html">General</a>
+                            </li>
+                            <li><a href="index2.html">Características</a>
+                            </li>
+                            <li><a href="index3.html">Colaboradores</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                </ul>
+
+            </div>
+            <!--/.nav-collapse -->
+
+</div>
