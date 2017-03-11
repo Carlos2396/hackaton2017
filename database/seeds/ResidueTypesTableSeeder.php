75eeed7e41fb3969add8b07de34eb7d9d43a9dc4
Angel Roberto Ruiz Mendoza <Angel Roberto Ruiz Mendoza>
2017-03-10 21:14:49 -0600

    Se agregaron las imagenes a los seeds de residue :)

--- database/seeds/ResidueTypesTableSeeder.php
+++ database/seeds/ResidueTypesTableSeeder.php
@@ -14,25 +14,25 @@ class ResidueTypesTableSeeder extends Seeder
         DB::table('residue_types')->insert([
         	'name' => 'PET',
         	'description' => 'Botellas de plástico, frascos de plástico, cintas de video, radiografías, etc',
-        	'image' => '/img/pet.png',
+        	'image' => 'pet.png',
         ]);
 
         DB::table('residue_types')->insert([
-        	'name' => 'Papel',
+        	'name' => 'Papel/Carton',
         	'description' => 'Carton, hojas de papel, servilletas, toallas de papel, etc. PAPEL DE BAÑO NO',
-        	'image' => '/img/pet.png',
+        	'image' => 'carton.png',
         ]);
 
         DB::table('residue_types')->insert([
         	'name' => 'Aluminio',
         	'description' => 'Papel aluminio, latas de refresco, latas de no perecederos, bandejas de aluminio, etc',
-        	'image' => '/img/pet.png',
+        	'image' => 'aluminio.png',
         ]);
 
         DB::table('residue_types')->insert([
         	'name' => 'Vidrio',
         	'description' => 'Botellas de cristal, tarros de cristal, vasos, etc. NINGUN TIPO DE CONTENEDOR CON RESIDUOS PELIGROSOS',
-        	'image' => '/img/pet.png',
+        	'image' => 'cristal.png',
         ]);
     }
 }
